675685fb9a45bd8076fac977eb65330f24fa930b
adkunw <aditya.kunto.wisnuraharja@gmail.com>
2024-09-12 10:11:02 +0700

    day 4 - penambahan contoh function dengan nilai return

--- function.js
+++ function.js
@@ -1,6 +1,16 @@
+//FUNCTION NON RETURN
 function generate_nama(nama, mobile) {
   console.log(`Nama saya adalah ${nama}, nomor telp saya adalah ${mobile}`);
 }
 
 generate_nama("Aditya", "081320100407");
 generate_nama("Zakiy", "0813123123123");
+
+//FUNCTION RETURN
+function generate_nama_return(nama, mobile) {
+  const text = `Nama saya adalah ${nama}, nomor telp saya adalah ${mobile}`;
+  return text;
+}
+
+const result = generate_nama_return("Kunto", "081987987987");
+console.log(result);
